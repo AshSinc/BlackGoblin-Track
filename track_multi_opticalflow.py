a1a23229e81a7597ff8b6e60ddce4752179e5cfd
Ashleigh <ashleighsinclair86@gmail.com>
2023-02-13 15:00:25 +0000

    testing optical flow

--- track_multi_opticalflow.py
+++ track_multi_opticalflow.py
@@ -3,11 +3,12 @@ import cv2
 import sys
 import getopt
 import os.path
+import numpy as np
  
 argv = sys.argv[1:]
 opts, args = getopt.getopt(argv, 'i:s:e:n:')
 
-VID_PATH = "./resources/People Walking Free Stock Footage2.mp4"
+VID_PATH = "./resources/People Walking Free Stock Footage.mp4"
 START_TIME = 0
 END_TIME = math.inf
 NUM_TO_TRACK = 1
@@ -75,6 +76,12 @@ for x in range(0,NUM_TO_TRACK):
     trackers.append(cv2.TrackerCSRT_create())
     trackers[x].init(frame, bboxes[x])
 
+first_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# Creates a mask image with zeros
+mask = np.zeros_like(frame)
+# Saturation for mask image is set to 255
+mask[..., 1] = 255
+
 # Start tracking
 print("starting")
 while (video.get(cv2.CAP_PROP_POS_MSEC) <= END_TIME):
@@ -85,19 +92,42 @@ while (video.get(cv2.CAP_PROP_POS_MSEC) <= END_TIME):
         print('something went wrong')
         break
 
+    # Each frame is converted into grayscale
+    current_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # optical flow using Farneback method
+    optical_flow = cv2.calcOpticalFlowFarneback(first_gray, current_gray,
+                    None,
+                    0.2, 4, 12, 2, 3, 1.1, 0)
+    
+    # Magnitude and Angle of 2D vector is calculated
+    mag, ang = cv2.cartToPolar(optical_flow[..., 0], optical_flow[..., 1])
+    
+    # Determines mask value using normalized magnitude
+    mask[..., 0] = ang * 180 / np.pi / 2
+    mask[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    
+    # Converts HSV (Hue Saturation Value) to RGB (or BGR)
+    rgb_frame = cv2.cvtColor(mask, cv2.COLOR_HSV2BGR)
+    
+    # Displays new output frame
+    #cv2.imshow("optical flow", rgb_frame)
+    
+    first_gray = current_gray
+
     for tracker in trackers:
-        success, bbox = tracker.update(frame)
+        success, bbox = tracker.update(rgb_frame)
 
         if success:
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+            cv2.rectangle(rgb_frame, p1, p2, (255,0,0), 2, 1)
         else:
-            cv2.putText(frame, "Tracking failure detected", (100,80), 
+            cv2.putText(rgb_frame, "Tracking failure detected", (100,80), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
             
-    cv2.imshow("Tracking", frame)
-    output.write(frame)
+    cv2.imshow("Tracking", rgb_frame)
+    output.write(rgb_frame)
     k = cv2.waitKey(1) & 0xff
     if k == 27 : break
         
